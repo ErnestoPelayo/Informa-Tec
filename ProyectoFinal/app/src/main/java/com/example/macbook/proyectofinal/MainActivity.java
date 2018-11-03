e74ab55a00bd5433f0cfb82c4b2484e80f36c639
Ernesto Pelayo <ernesto.p@itemexicali.edu.mx>
2018-11-03 15:32:17 -0700

    proyecto guardado

---
+++ ProyectoFinal/app/src/main/java/com/example/macbook/proyectofinal/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.macbook.proyectofinal;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    selectedFragment = new ConferenceFragment();
+                    break;
+                case R.id.navigation_contest:
+                    selectedFragment = new ContestFragment();
+                    break;
+                case R.id.navigation_conferences:
+                    selectedFragment = new HomeFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConferenceFragment()).commit();
+        }
+    }
+
+
+
+
+}
